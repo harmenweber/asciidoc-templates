c3858f8d9b3609e06e8635a11cd173e35ad72195
Harmen Weber <weber@arviem.com>
2022-02-11 08:44:24 +0100

    Started a nord theme and amended the simple-article.adoc with more examples so that I can verify the nord theme.

---
+++ scripts/create-pdf-with-nord-theme.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Exit on error. Append "|| true" if you expect an error.
+set -o errexit
+# Exit on error inside any functions or subshells.
+set -o errtrace
+# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
+set -o nounset
+# Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
+set -o pipefail
+# Turn on traces, useful while debugging but commented out by default
+# set -o xtrace
+
+# Collect information about the running script.
+readonly scriptFile="$(basename "${0}")"
+readonly scriptName="$(basename "${0}" .sh)"
+readonly scriptHome="$(cd $(dirname "${0}") && pwd)"
+
+# Constants
+readonly themesHome="${scriptHome}/../themes"
+readonly fontsHome="${scriptHome}/../fonts"
+readonly fontsListSeparatedBySemicolon="$(find "${fontsHome}" -depth 1 | tr '\n' \;)"
+
+asciidoctor-pdf \
+    -a pdf-theme="${themesHome}/nord-theme.yml" \
+    -a pdf-fontsdir="${fontsListSeparatedBySemicolon};GEM_FONTS_DIR" \
+    "$@"
